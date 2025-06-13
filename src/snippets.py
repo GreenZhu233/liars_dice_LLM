e18309942832136633dfc7a3c9ed891e3478fbb8
NianBai666 <2200011165@stu.pku.edu.cn>
2025-06-13 22:59:56 +0800

    [feat] 添加对gemini-2.0-flash模型的API密钥和URL支持

--- src/snippets.py
+++ src/snippets.py
@@ -1,11 +1,19 @@
 model_to_key_name = {
     "deepseek-chat": "DEEPSEEK_API_KEY",
     "deepseek-reasoner": "DEEPSEEK_API_KEY",
-    "doubao-1-5-lite-32k-250115": "DOUBAO_API_KEY"
+    "doubao-1-5-lite-32k-250115": "DOUBAO_API_KEY",
+    "gemini-2.0-flash": "GOOGLE_API_KEY"
 }
 
 model_to_url = {
     "deepseek-chat": "https://api.deepseek.com",
     "deepseek-reasoner": "https://api.deepseek.com",
     "doubao-1-5-lite-32k-250115": "https://ark.cn-beijing.volces.com/api/v3"
+}
+
+model_to_API = {
+    "deepseek-chat": "OpenAI",
+    "deepseek-reasoner": "OpenAI",
+    "doubao-1-5-lite-32k-250115": "OpenAI",
+    "gemini-2.0-flash": "Google"
 }
