4ac82fa1b5646b273d943ce844e1df56399e995c
GreenZhu233 <greenzhu233@foxmail.com>
2025-06-20 00:03:35 +0800

    去除qwq-plus模型

--- src/snippets.py
+++ src/snippets.py
@@ -5,7 +5,6 @@ model_list = [
     "doubao-1-5-thinking-pro-250415",
     "gemini-2.5-flash-preview-05-20",
     "glm-z1-flash",
-    "qwq-plus",
     "qwen-max-0125",
     "hunyuan-t1-latest",
     "x1"
@@ -18,7 +17,6 @@ model_to_key_name = {
     "doubao-1-5-thinking-pro-250415": "DOUBAO_API_KEY",
     "gemini-2.5-flash-preview-05-20": "GEMINI_API_KEY",
     "glm-z1-flash": "ZHIPU_API_KEY",
-    "qwq-plus": "DASHSCOPE_API_KEY",
     "qwen-max-0125": "DASHSCOPE_API_KEY",
     "hunyuan-t1-latest": "HUNYUAN_API_KEY",
     "x1": "SPARK_API_KEY"
@@ -30,7 +28,6 @@ model_to_url = {
     "doubao-1-5-lite-32k-250115": "https://ark.cn-beijing.volces.com/api/v3",
     "doubao-1-5-thinking-pro-250415": "https://ark.cn-beijing.volces.com/api/v3",
     "glm-z1-flash": "https://open.bigmodel.cn/api/paas/v4/",
-    "qwq-plus": "https://dashscope.aliyuncs.com/compatible-mode/v1",
     "qwen-max-0125": "https://dashscope.aliyuncs.com/compatible-mode/v1",
     "hunyuan-t1-latest": "https://api.hunyuan.cloud.tencent.com/v1",
     "x1": "https://spark-api-open.xf-yun.com/v2"
@@ -43,7 +40,6 @@ model_to_API = {
     "doubao-1-5-thinking-pro-250415": "OpenAI",
     "gemini-2.5-flash-preview-05-20": "Google",
     "glm-z1-flash": "OpenAI",
-    "qwq-plus": "OpenAI",
     "qwen-max-0125": "OpenAI",
     "hunyuan-t1-latest": "OpenAI",
     "x1": "OpenAI"
